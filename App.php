20ac91f7559b30edb29f0a8c37703dc0437a786f
Fadhil <fadhilahdwi.a@gmail.com>
2025-06-11 08:04:12 +0700

    fix: correct path separators in require_once statements

--- App.php
+++ App.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once __DIR__ . 'models/TodoList.php';
-require_once __DIR__ . 'controllers/ShowTodoList.php';
-require_once __DIR__ . 'controllers/AddTodoList.php';
-require_once __DIR__ . 'controllers/RemoveTodoList.php';
-require_once __DIR__ . 'views/ViewShowTodoList.php';
-require_once __DIR__ . 'views/ViewAddTodoList.php';
-require_once __DIR__ . 'views/ViewRemoveTodoList.php';
-require_once __DIR__ . 'helpers/input.php';
+require_once __DIR__ . '/models/TodoList.php';
+require_once __DIR__ . '/controllers/ShowTodoList.php';
+require_once __DIR__ . '/controllers/AddTodoList.php';
+require_once __DIR__ . '/controllers/RemoveTodoList.php';
+require_once __DIR__ . '/views/ViewShowTodoList.php';
+require_once __DIR__ . '/views/ViewAddTodoList.php';
+require_once __DIR__ . '/views/ViewRemoveTodoList.php';
+require_once __DIR__ . '/helpers/input.php';
 
 echo "Aplikasi TodoList" . PHP_EOL;
 echo "===================" . PHP_EOL;
